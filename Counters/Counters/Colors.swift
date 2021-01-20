ac81130559d40af16e83b6f51abe642768cad21e
smara <smara128@gmail.com>
2021-01-20 12:29:05 -0300

    Add icons e colors for Welcome screen.

---
+++ Counters/Counters/Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Colors.swift
+//  Counters
+//
+//  Created by Me on 19/01/21.
+//
+
+import UIKit
+
+class Colors {
+
+    static public let counterOrange = #colorLiteral(red: 1, green: 0.5843137255, blue: 0, alpha: 1) // system orange
+    
+    static public let counterRed = #colorLiteral(red: 1, green: 0.231372549, blue: 0.1882352941, alpha: 1) // system red
+    
+    static public let counterYellow = #colorLiteral(red: 1, green: 0.8, blue: 0, alpha: 1) // system yellow
+    
+    static public let accentGreen = UIColor(named: "accentGreen")
+
+    static public let subtitleDarkGray = UIColor(named: "subtitleDarkGray") // 2B2B2B  // welcome summary subtitle
+
+    static public let subtitleMediumGray = UIColor(named: "subtitleMediumGray") // 4A4A4A  // welcome subheader
+
+    
+}
+
